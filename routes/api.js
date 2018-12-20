f19dae2785a3c9832e6e38cd09eba6a9482f29d4
Glitch (gratis-tern) <none>
2018-12-20 18:23:23 +0000

    💸✍️ Checkpoint ./routes/api.js:2177980/5685 ./package.json:2177980/472

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,134 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+const fetch = require('node-fetch');
+const DATABASE = process.env.DATABASE;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+module.exports = function (app, db) {
+  MongoClient.connect(DATABASE, function(err, db) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected');
 
-module.exports = function (app) {
+      app.route('/api/stock-prices')
+        .get(function (req, res){
+          const query = {
+            stock: req.query.stock,
+            like: req.query.like ? 1 : 0,
+            ip: req.headers['x-forwarded-for'].split(',')[0]
+          };
+        
+          if (!query.stock) {
+            res.redirect('/');
+          }
+        
+          if (typeof query.stock == 'string') {
+            fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${query.stock}&apikey=${process.env.AV_KEY}`)
+              .then(data => data.json())
+              .catch(err => console.log(err))
+              .then(data => {
+                const stock = {
+                  symbol: data['Global Quote']['01. symbol'],
+                  price: data['Global Quote']['05. price']
+                }
+                
+                db.collection('stocks').findOne({symbol: stock.symbol}, (err, doc) => {
+                  if (err) console.log(err);
+                  else {
+                    const allowLike = !(doc && doc.ip && doc.ip.includes(query.ip));
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+                    db.collection('stocks').findAndModify(
+                      {symbol: stock.symbol},
+                      {},
+                      {$setOnInsert:{
+                        symbol: stock.symbol,
+                        price: stock.price
+                      },$set:{
+                        last_update: new Date()
+                      },$addToSet:{
+                        ip: query.like && query.ip
+                      },$inc:{
+                        likes: (query.like && allowLike) ? 1 : 0
+                      }},
+                      {upsert:true, new: true},
+                      (err, doc) => {
+                        if (err) console.log(err)
+                        else {
+                          res.json({stockData: {stock: doc.value.symbol, price: doc.value.price, likes: doc.value.likes}});
+                        }                      
+                      }
+                    );
+                  }
+                });
+                            
+              }).catch(err => {console.log(err)});
+            
+          } else {
+            const stocksPromise = [];
+            query.stock.map(stock => stocksPromise.push(fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${process.env.AV_KEY}`)));
+            
+            Promise.all(stocksPromise)
+              .then((res, i) => {
+                const jsonPromise = [];
+                res.map(res => jsonPromise.push(res.json()));
+                return Promise.all(jsonPromise);
+              })
+              .catch(err => {console.log(err)})
+              .then((data) => {
+                const stocks = data.map(data => ({
+                  symbol: data['Global Quote']['01. symbol'],
+                  price: data['Global Quote']['05. price'],
+                  like: query.like ? 1 : 0,
+                  likes: 0
+                }))
+              
+              
+                stocks.map((stock, i) => {
+                  db.collection('stocks').findOne({symbol: stock.symbol}, (err, doc) => {
+                    if (err) console.log(err)
+                    else {
+                      const allowLike = !(doc && doc.ip && doc.ip.includes(query.ip));
+
+                      db.collection('stocks').findAndModify(
+                        {symbol: stock.symbol},
+                        {},
+                        {$setOnInsert:{
+                          symbol: stock.symbol,
+                          price: stock.price
+                        },$set:{
+                          last_update: new Date()
+                        },$addToSet:{
+                          ip: query.like && query.ip
+                        },$inc:{
+                          likes: (stock.like && allowLike) ? 1 : 0
+                        }},
+                        {upsert:true, new: true},
+                        (err, doc) => {
+                          if (err) console.log(err)
+                          else {
+                            stocks[i].likes = doc.value.likes;
+
+                            if (i == stocks.length - 1) {
+                              const response = {};
+                              response.stockData = stocks.map((stock, i) => ({
+                                stock: stock.symbol, price: stock.price, rel_likes: stock.likes - stocks[(i + 1) % 2].likes
+                              }));
+                              res.json(response);
+                            }
+                          }                      
+                        }
+                      );
+                    }
+                  });
+                  
+                });
+              
+              }).catch(err => {console.log(err)});
+            }
+        });
+    }  
+  });
 };
+
